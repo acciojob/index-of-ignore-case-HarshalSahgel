6b1671897b0aa2c61b6fe57ab4e9fe31d20edc2e
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-01-19 00:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+	let a1 = s1.toLowerCase();
+	let a2 = s2.toLowerCase();
+	
+  if(a2 === 0){
+	  return -1;
+  }
+let substr = a1.indexOf(a2);
+
+	if(substr !== -1){
+		return substr;
+	}
+	else{
+		return -1;
+	}
 }
 
-// Please do not change the code below
+//  
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
